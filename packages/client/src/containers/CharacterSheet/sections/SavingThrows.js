496585751d59a13b9ee290ceaf5d97cf45e71ee9
br-ndt <nlax76@gmail.com>
2022-06-15 17:15:19 -0400

    ree

--- packages/client/src/containers/CharacterSheet/sections/SavingThrows.js
+++ packages/client/src/containers/CharacterSheet/sections/SavingThrows.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import characterContext from "../../../contexts/characterContext";
 import SaveThrow from "../entries/SaveEntry";
 import Collapsible from "../../EventCallers/Collapsible";
-import attributes from "../../../constants/attributes.js";
+import attributes from "../../../constants/attributeList.js";
 
 const CharacterSavingThrows = () => {
   const proficiencies = useContext(characterContext).proficiency;
