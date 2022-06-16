7c0b934a1ee2ff87cff7e5ee3acafb18c0607f40
br-ndt <nlax76@gmail.com>
2022-06-16 16:46:17 -0400

    add path.join

--- packages/server/config/clientStaticPath.js
+++ packages/server/config/clientStaticPath.js
@@ -3,7 +3,7 @@ import path, { dirname } from "path";
 
 const clientStaticPath = () => {
   const currentPath = dirname(fileURLToPath(import.meta.url));
-  let staticPath = "../../client/public";
+  let staticPath = path.join(currentPath, "../../client/public");
   if (process.env.NODE_ENV === "production") {
     staticPath = path.join(currentPath, "../client");
   }
