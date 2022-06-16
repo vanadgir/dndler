71caa6110218cfe5cf7bff2e535ab2b90868ef8e
br-ndt <nlax76@gmail.com>
2022-06-16 16:44:53 -0400

    modify client locations for pipeline

--- packages/server/config/clientStaticPath.js
+++ packages/server/config/clientStaticPath.js
@@ -3,7 +3,7 @@ import path, { dirname } from "path";
 
 const clientStaticPath = () => {
   const currentPath = dirname(fileURLToPath(import.meta.url));
-  let staticPath = path.join(currentPath, "../../client/build");
+  let staticPath = "../../client/public";
   if (process.env.NODE_ENV === "production") {
     staticPath = path.join(currentPath, "../client");
   }
