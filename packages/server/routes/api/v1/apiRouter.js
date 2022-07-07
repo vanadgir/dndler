54d5f4267f9c9ce4fa68a5eee65718cd61fab506
br-ndt <nlax76@gmail.com>
2022-07-07 15:36:32 -0400

    add static database endpoints to apiRouter

--- packages/server/routes/api/v1/apiRouter.js
+++ packages/server/routes/api/v1/apiRouter.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { sourcebooks, names, backgrounds, races } from "../../../data/data.js";
+import { sourcebooks, names, backgrounds, races } from "../../../db/seeders/SRD/data.js";
+import Attribute from "../../../db/models/Attribute.js";
+import Spell from "../../../db/models/Spell.js";
+import Equipment from "../../../db/models/Equipment.js";
+import Skill from "../../../db/models/Skill.js";
 import characterRouter from "./character/characterRouter.js";
 import charIndexRouter from "./characters/charIndexRouter.js";
 
@@ -20,6 +24,26 @@ apiRouter.get("/backgrounds", (req, res) => {
 apiRouter.get("/races", (req, res) => {
   res.status(200).send(JSON.stringify(races)).end();
 });
+
+apiRouter.get("/attributes", async (req, res) => {
+  const attributes = await Attribute.query();
+  res.status(200).send(JSON.stringify(attributes)).end();
+})
+
+apiRouter.get("/skills", async (req, res) => {
+  const skills = await Skill.query();
+  res.status(200).send(JSON.stringify(skills)).end();
+})
+
+apiRouter.get("/equipments", async (req, res) => {
+  const equipments = await Equipment.query();
+  res.status(200).send(JSON.stringify(equipments)).end();
+})
+
+apiRouter.get("/spells", async (req, res) => {
+  const spells = await Spell.query();
+  res.status(200).send(JSON.stringify(spells)).end();
+})
 
 apiRouter.use("/character", characterRouter);
 apiRouter.use("/characters", charIndexRouter);
