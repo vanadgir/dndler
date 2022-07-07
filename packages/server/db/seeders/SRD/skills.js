d3ad3189b4e149879d42c736adf310cdd680c409
br-ndt <nlax76@gmail.com>
2022-07-07 15:36:43 -0400

    add skills.js datafile

---
+++ packages/server/db/seeders/SRD/skills.js
@@ -0,0 +1,72 @@
+const skills = [
+  {
+    title: "Acrobatics",
+    attributeId: "DEX"
+  },
+  {
+    title: "Animal Handling",
+    attributeId: "WIS"
+  },
+  {
+    title: "Arcana",
+    attributeId: "INT"
+  },
+  {
+    title: "Athletics",
+    attributeId: "STR"
+  },
+  {
+    title: "Deception",
+    attributeId: "CHA"
+  },
+  {
+    title: "Insight",
+    attributeId: "WIS"
+  },
+  {
+    title: "Intimidation",
+    attributeId: "CHA"
+  },
+  {
+    title: "Investigation",
+    attributeId: "INT"
+  },
+  {
+    title: "History",
+    attributeId: "INT"
+  },
+  {
+    title: "Medicine",
+    attributeId: "WIS"
+  },
+  {
+    title: "Nature",
+    attributeId: "INT"
+  },
+  {
+    title: "Perception",
+    attributeId: "WIS"
+  },
+  {
+    title: "Performance",
+    attributeId: "CHA"
+  },
+  {
+    title: "Persuasion",
+    attributeId: "CHA"
+  },
+  {
+    title: "Sleight of Hand",
+    attributeId: "DEX"
+  },
+  {
+    title: "Stealth",
+    attributeId: "DEX"
+  },
+  {
+    title: "Survival",
+    attributeId: "WIS"
+  },
+]
+
+export default skills;
