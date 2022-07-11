bc5f4513125b7ad406deee61eb49e54c8c1f16f1
br-ndt <nlax76@gmail.com>
2022-07-11 17:57:16 -0400

    comment out ClassSeeder equipment section - need better means of handling equipment choices

--- packages/server/db/seeders/ClassSeeder.js
+++ packages/server/db/seeders/ClassSeeder.js
@@ -42,57 +42,57 @@ class ClassSeeder {
             }
           }
         }
-        console.log("seeding equipments into", singleClassTitle, "...");
-        for (const thisEquipment of singleClassData.Equipment) {
-          if (Array.isArray(thisEquipment)) {
-            console.log(thisEquipment[0]);
-            const equipment1Model = await Equipment.query().findOne({
-              title: thisEquipment[0].split("(")[0],
-            });
-            const equipment2Model = await Equipment.query().findOne({
-              title: thisEquipment[1].split("(")[0],
-            });
-            if(equipment1Model && equipment2Model) {
-              let count1 = 1;
-              const ifCount1 = thisEquipment[0].split("(")[1];
-              if(ifCount1) {
-                count1 = parseInt(ifCount1[0]);
-              }
-              let count2 = 1;
-              const ifCount2 = thisEquipment[1].split("(")[1];
-              if(ifCount2) {
-                count2 = parseInt(ifCount2[0]);
-              }
-              await ClassEquipment.query().insert({
-                classId: thisClass.id,
-                equipment1Id: equipment1Model.id,
-                equipment1Count: count1,
-                equipment2Id: equipment2Model.id,
-                equipment2Count: count2
-              })
-            }
-          } else {
-            const equipmentModel = await Equipment.query().findOne({
-              title: thisEquipment.split("(")[0],
-            });
-            if(!equipmentModel) {
-              console.log(
-                `the Equipment of ${thisEquipment} was not found in the equipments table`
-              );
-            } else {
-              let count = 1;
-              let ifCount = thisEquipment.split("(")[1];
-              if(ifCount) {
-                count = parseInt(ifCount[0]);
-              }
-              await ClassEquipment.query().insert({
-                classId: thisClass.id,
-                equipment1Id: equipmentModel.id,
-                equipment1Count: count
-              })
-            }
-          }
-        }
+        // console.log("seeding equipments into", singleClassTitle, "...");
+        // for (const thisEquipment of singleClassData.Equipment) {
+        //   if (Array.isArray(thisEquipment)) {
+        //     console.log(thisEquipment[0]);
+        //     const equipment1Model = await Equipment.query().findOne({
+        //       title: thisEquipment[0].split("(")[0],
+        //     });
+        //     const equipment2Model = await Equipment.query().findOne({
+        //       title: thisEquipment[1].split("(")[0],
+        //     });
+        //     if(equipment1Model && equipment2Model) {
+        //       let count1 = 1;
+        //       const ifCount1 = thisEquipment[0].split("(")[1];
+        //       if(ifCount1) {
+        //         count1 = parseInt(ifCount1[0]);
+        //       }
+        //       let count2 = 1;
+        //       const ifCount2 = thisEquipment[1].split("(")[1];
+        //       if(ifCount2) {
+        //         count2 = parseInt(ifCount2[0]);
+        //       }
+        //       await ClassEquipment.query().insert({
+        //         classId: thisClass.id,
+        //         equipment1Id: equipment1Model.id,
+        //         equipment1Count: count1,
+        //         equipment2Id: equipment2Model.id,
+        //         equipment2Count: count2
+        //       })
+        //     }
+        //   } else {
+        //     const equipmentModel = await Equipment.query().findOne({
+        //       title: thisEquipment.split("(")[0],
+        //     });
+        //     if(!equipmentModel) {
+        //       console.log(
+        //         `the Equipment of ${thisEquipment} was not found in the equipments table`
+        //       );
+        //     } else {
+        //       let count = 1;
+        //       let ifCount = thisEquipment.split("(")[1];
+        //       if(ifCount) {
+        //         count = parseInt(ifCount[0]);
+        //       }
+        //       await ClassEquipment.query().insert({
+        //         classId: thisClass.id,
+        //         equipment1Id: equipmentModel.id,
+        //         equipment1Count: count
+        //       })
+        //     }
+        //   }
+        // }
         console.log("seeding features into", singleClassTitle, "...");
         for (const thisLevel of Object.keys(singleClassData.Features)) {
           for (const thisFeature of singleClassData.Features[thisLevel]) {
