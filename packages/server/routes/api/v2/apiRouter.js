0b5f7485e27807ec0670493534e9b9284ef46182
Varun Nadgir <theminivann@gmail.com>
2023-10-04 18:24:15 -0400

    additional data routes

--- packages/server/routes/api/v2/apiRouter.js
+++ packages/server/routes/api/v2/apiRouter.js
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import { generateAll } from "../../../generators/generateAll.js";
 
 const apiRouter_v2 = express.Router();
 
-apiRouter_v2.get("/nuddermin", cors(), (req, res) => {
+let items = [];
+let counts = {};
+
+apiRouter_v2.use(cors()); // Enable CORS for all routes
+apiRouter_v2.use(bodyParser.json());
+
+apiRouter_v2.get("/nuddermin", (req, res) => {
   const generatedChar = generateAll([]);
   const keepKeys = ['name', 'background', 'class', 'race', 'stats'];
 
@@ -13,6 +20,28 @@ apiRouter_v2.get("/nuddermin", cors(), (req, res) => {
   );
 
   res.status(200).send(JSON.stringify(filteredCharacter)).end();
+});
+
+apiRouter_v2.put('/items', (req, res) => {
+  items = req.body;
+
+  res.json({ message: 'PUT request successful' });
+});
+
+apiRouter_v2.get('/items', (req, res) => {
+
+  res.status(200).send(items).end();
+});
+
+apiRouter_v2.put('/items/counts', (req, res) => {
+  counts = req.body;
+
+  res.json({ message: 'PUT request successful' });
+});
+
+apiRouter_v2.get('/items/counts', (req, res) => {
+
+  res.status(200).send(counts).end();
 });
 
 export default apiRouter_v2;
