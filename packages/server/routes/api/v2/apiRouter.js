efb43f8dbf278bc795621e1eb896d64f087abb80
Varun Nadgir <theminivann@gmail.com>
2023-10-03 21:52:59 -0400

    filtered character

--- packages/server/routes/api/v2/apiRouter.js
+++ packages/server/routes/api/v2/apiRouter.js
@@ -5,7 +5,14 @@ import { generateAll } from "../../../generators/generateAll.js";
 const apiRouter_v2 = express.Router();
 
 apiRouter_v2.get("/nuddermin", cors(), (req, res) => {
-  res.status(200).send(JSON.stringify(generateAll([]))).end();
+  const generatedChar = generateAll([]);
+  const keepKeys = ['name', 'background', 'class', 'race', 'stats'];
+
+  const filteredCharacter = Object.fromEntries(
+    Object.entries(generatedChar).filter(([key]) => keepKeys.includes(key))
+  );
+
+  res.status(200).send(JSON.stringify(filteredCharacter)).end();
 });
 
 export default apiRouter_v2;
